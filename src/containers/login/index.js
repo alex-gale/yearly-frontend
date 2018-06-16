5da449d9bb8530bc2f3ca11ad36517617f6914f7
Alex Gale <alexgaleiii@gmail.com>
2018-06-16 18:59:37 +0100

    Add card component
    * Added card component

--- src/containers/login/index.js
+++ src/containers/login/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 import './index.scss'
+import Card from '../../components/card'
 
 const Login = () => {
 	return (
 		<div className="login-content">
-			<h1>Login</h1>
+			<Card>
+				<h1>Welcome back to <span className="yearly-title">Yearly</span></h1>
+			</Card>
 		</div>
 	)
 }
